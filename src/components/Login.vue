3915bd88c9e878d29d66cfd84e3807d020dce906
hucongcong <flycc258@163.com>
2018-12-20 18:04:12 +0800

    登录功能完成了

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,60 +1,114 @@
 <template>
   <div class="login">
     <!--
-      el-form：整个form组件
-      :model="form" ： 必须提供的对象
-      label-width="80px"： label宽度
+      el-form: 整个form组件
+      :model： 用于收集整个表单的值  每个表单元素应该和form中的属性双向绑定
+      label-width="80px"  设置label的宽度
+      el-form-item： 一个表单项
 
-      el-form-item: 一个表单项
-
-      element-ui中所有的组件在渲染的时候，这个组件名其实就是类名
-     -->
-    <el-form ref="form" :model="form" label-width="80px">
+      表单校验
+        1. 给el-form传入一个rules规则
+        2. 要给el-form-item 添加一个prop属性，值就是字段名
+    -->
+    <el-form status-icon ref="form" :rules="rules" :model="form" label-width="80px">
       <img src="@/assets/avatar.jpg" alt="">
-      <el-form-item label="用户名">
-        <el-input v-model="form.username"></el-input>
+      <el-form-item label="用户名" prop="username">
+        <el-input v-model="form.username" placeholder="请输入用户名"></el-input>
       </el-form-item>
-       <el-form-item label="密码">
-        <el-input v-model="form.password"></el-input>
+      <el-form-item label="密码" prop="password">
+        <el-input v-model="form.password" type="password" placeholder="请输入密码"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">立即创建</el-button>
-        <el-button>取消</el-button>
+        <el-button type="primary" @click="login">登录</el-button>
+        <el-button @click="reset">重置</el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
 
 <script>
+// 导入axios
+import axios from 'axios'
 export default {
-  data () {
+  data() {
     return {
+      // 收集表单数据
       form: {
         username: '',
         password: ''
+      },
+      // 表单的校验规则
+      rules: {
+        // 用户名的校验规则
+        username: [
+          // 非空校验 trigger： blur  change
+          { required: true, message: '用户名不能为空', trigger: 'change' },
+          { min: 3, max: 9, message: '用户长度在 3 到 9 个字符', trigger: 'change' }
+        ],
+        password: [
+          // 非空校验
+          { required: true, message: '密码不能为空', trigger: 'change' },
+          { min: 6, max: 12, message: '用户长度在 6 到 12 个字符', trigger: 'change' }
+        ]
       }
     }
   },
   methods: {
-    onSubmit () {
-      console.log('submit!')
+    reset() {
+      // 获取到整个表单组件，重置
+      this.$refs.form.resetFields()
+    },
+    login() {
+      // 让整个表单校验
+      this.$refs.form.validate(valid => {
+        // valid如果为true，就表示通过 否则不通过
+        if (valid) {
+          // 发送ajax请求，进行登录
+          axios({
+            method: 'post',
+            url: 'http://localhost:8888/api/private/v1/login',
+            data: this.form
+          }).then(res => {
+            console.log(res.data)
+            if (res.data.meta.status === 200) {
+              // alert('登录成功')
+              this.$message.success('登录成功')
+              // 把后台颁发的token存起来
+              localStorage.setItem('token', res.data.data.token)
+              // 跳转到Home组件
+              // 参数：跳转的路径
+              // 类似于location.href
+              this.$router.push('/home')
+            } else {
+              // 失败的消息  this.$message: 弹出一个消息提示
+              this.$message({
+                message: res.data.meta.msg,
+                type: 'error',
+                duration: 1000
+              })
+            }
+          })
+        } else {
+          return false
+        }
+      })
     }
   }
 }
 </script>
 
 <style lang="less">
-// 注意：vue-cli把所有的loader都配置好了，但是没有把依赖项全部都下载下来
 .login {
-  height: 100%;
   width: 100%;
-  background-color:  #2d434c;
+  height: 100%;
+  background: #2d434c;
   overflow: hidden;
   .el-form {
     width: 400px;
-    background-color: #fff;
     margin: 200px auto;
+    background-color: #fff;
     padding: 75px 40px 15px;
+    border-radius: 10px;
     position: relative;
     img {
       position: absolute;
@@ -63,6 +117,10 @@ export default {
       top: -70px;
       border-radius: 50%;
       border: 10px solid #fff;
+    }
+
+    .el-button:nth-child(2) {
+      margin-left: 80px;
     }
   }
 }
